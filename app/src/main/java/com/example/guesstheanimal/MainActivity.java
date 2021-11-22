281eac0993d75dc57092dde98bb2eb223baf46f8
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-22 07:36:23 -0500

    Add UI and DownloadTask class

--- app/src/main/java/com/example/guesstheanimal/MainActivity.java
+++ app/src/main/java/com/example/guesstheanimal/MainActivity.java
@@ -2,15 +2,53 @@ package com.example.guesstheanimal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     // Where to get the animals: https://www.listchallenges.com/100-animals
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Download images
+
     }
 }
