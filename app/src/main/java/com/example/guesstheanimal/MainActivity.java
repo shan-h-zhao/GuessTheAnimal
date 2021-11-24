148d686e042a1917f8b98a1f0102d2bb99a309aa
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-24 16:19:00 -0500

    Match pattern & find out the urls

--- app/src/main/java/com/example/guesstheanimal/MainActivity.java
+++ app/src/main/java/com/example/guesstheanimal/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.guesstheanimal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                     result += current;
                     data = reader.read();
                 }
+                return result;
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -49,6 +53,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Download images
+        DownloadTask downloadTask = new DownloadTask();
+        String result = null;
+
+        try {
+            result = downloadTask.execute("https://www.listchallenges.com/100-animals").get();
+            String[] splitResult = result.split("checklist-listItems clearfix");
+            String[] splitResult1 = splitResult[1].split("checklist-itemsSectionRight");
+
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult1[0]);
+
+            while (m.find()) {
+                String firstLetters = m.group(1).substring(0, 8);
+                if (firstLetters.equals("/f/items")) {
+                    // append https://www.listchallenges.com/ to the beginning
+                    System.out.println("https://www.listchallenges.com/" + m.group(1));
+                }
+
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult1[0]);
+
+            while (m.find()) {
+                Log.i("animal name", "xxxxxxxx");
+                System.out.println(m.group(1));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("error","error");
+        }
 
     }
 }
